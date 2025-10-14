da81d293ad1254ba88895f58448bccbc73b66ac9
amirmahdi <167899101+amirdevv8@users.noreply.github.com>
2025-10-14 15:08:36 +0330

    Add files via upload

---
+++ project3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // چک کردن شرط
+            if (number < 10 && number % 2 == 0)
+            {
+                Console.WriteLine("true");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("false");
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
---
+++ project4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        // امیرمهدی میرفخرایی
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (number % 7 == 0)
+            {
+                Console.WriteLine("yes");
+                Console.ReadKey();
+            }
+            else
+            {
+                int result = number * 3;
+                Console.WriteLine(result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
