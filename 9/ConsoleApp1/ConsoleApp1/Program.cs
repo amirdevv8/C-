746106bce4dfa03913fb539de7ba03f093c1db2f
amirmahdi <167899101+amirdevv8@users.noreply.github.com>
2025-11-04 23:02:14 +0330

    Add files via upload

---
+++ 9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number;
+        int position = -1;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"num {i}: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number == 2)
+            {
+                position = i;
+            }
+        }
+
+        if (position != -1)
+        {
+            Console.WriteLine($"position {position}.yes");
+        }
+        else
+        {
+            Console.WriteLine("eror");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ project8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//amirmahdi mirfakhraei
+            int number;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"number {i} enter a number : ");
+                number = int.Parse(Console.ReadLine());
+
+
+                if (number % 2 == 0)
+                    Console.WriteLine("even.");
+                else
+                    Console.WriteLine("odd");
+
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
