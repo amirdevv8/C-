86b73785993dc54b1406626243ab0b58990632c5
amirmahdi <167899101+amirdevv8@users.noreply.github.com>
2025-11-14 18:22:05 +0330

    Add files via upload

---
+++ project10/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static int Multiply(int a, int b)
+    {
+        if (b == 0)
+            return 0;
+
+        if (b < 0)
+            return -Multiply(a, -b);
+
+
+        return a + Multiply(a, b - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a one number : ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a tow number : ");
+        int b = int.Parse(Console.ReadLine());
+
+        int result = Multiply(a, b);
+
+        Console.WriteLine("حاصل ضرب = " + result);
+        Console.ReadKey();
+
+    }
+}
---
+++ project9/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+ 
+    static double Perimeter(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+
+    
+    static double Area(double a, double b, double c)
+    {
+        double s = (a + b + c) / 2; 
+        return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a one number : ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter a tow number : ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter a end number : ");
+        double c = double.Parse(Console.ReadLine());
+
+        double perimeter = Perimeter(a, b, c);
+        double area = Area(a, b, c);
+
+        Console.WriteLine("perimeter = " + perimeter);
+        Console.WriteLine("area = " + area);
+        Console.ReadKey();
+
+    }
+    
+        }
+
